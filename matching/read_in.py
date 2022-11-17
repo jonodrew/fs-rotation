f15d55de94f8931600840302014d989827be3db9
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-11-17 11:30:28 +0000

    Add function to prepare data for BaseClass model
    This change adds a function that prepares the dictionary for the instantiator of the BaseClass and subclasses

--- matching/read_in.py
+++ matching/read_in.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import csv
 import functools
-from typing import Sequence, Type
+from typing import Sequence, Type, Union
 
 from matching.models import Candidate, Role
 
@@ -17,6 +17,19 @@ def read_roles() -> Sequence[Role]:
     return _read_and_create_objects("roles.csv", Role)
 
 
+def _correct_line(line_dict: dict[str, str]) -> dict[str, Union[int, str]]:
+    fresh_dict = {}
+    skills_dict = {}
+    for key, value in line_dict.items():
+        if key in {"Digital", "Operations", "Finance", "Policy"}:
+            value = int(value)
+            skills_dict[key] = int(value)
+        else:
+            fresh_dict[key] = value
+    fresh_dict['skills'] = skills_dict
+    return fresh_dict
+
+
 def _read_and_create_objects(filepath: str, model: Type[Candidate] | Type[Role]) -> Sequence[Role | Candidate]:
     with open(filepath) as file:
-        return [model(**line) for line in csv.DictReader(file)]
+        return [model(_correct_line(**line)) for line in csv.DictReader(file)]
