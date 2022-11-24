5610d2a5c80fdef48e8abba551f100dcdc1c3e09
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-11-24 14:36:39 +0000

    Begin writing script to calculate matches

--- fast_stream_22/scripts/process_pairs.py
+++ fast_stream_22/scripts/process_pairs.py
@@ -1,6 +1,10 @@
 import click
+from click import echo
+from fast_stream_22.matching.match import Matching
+from fast_stream_22.matching.read_in import read_candidates, read_roles
 
 
 @click.command
 def process_matches():
-    pass
+    matches = Matching(read_candidates(), read_roles()).report_pairs()
+    echo(matches)
