19777f10ae3536fce9efbe6e3bfcac8c0ac3a83f
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2022-11-24 09:34:49 +0000

    Rewrite Candidate model

--- matching/models.py
+++ matching/models.py
@@ -1,8 +1,10 @@
 from enum import Enum
-from typing import Literal, Collection
+from typing import Literal, Sequence
 
 SkillLevel = Literal[0, 1, 2, 3, 4, 5]
 Skills = dict[str, SkillLevel]
+
+StrBool = Literal["true", "false"]
 
 
 class Clearance(Enum):
@@ -13,42 +15,58 @@ class Clearance(Enum):
 
 
 class BaseClass:
-    def __init__(self, name: str, clearance: Clearance, location: str, skills: Skills, email_address: str):
-        self.name = name
-        self._clearance = clearance
-        self._location = location
-        self._skills = skills
-        self._email = email_address
-
-
-class Candidate(BaseClass):
-    def __init__(self, name: str, clearance_held: Clearance, current_location: str, current_skills: Skills,
-                 email_address: str, can_relocate: bool, preferred_locations: Collection[str]):
-        super().__init__(name, clearance_held, current_location, current_skills, email_address)
-        self.can_relocate = can_relocate
-        self.preferred_locations = preferred_locations
+    def __init__(self, uid: str, clearance: str):
+        self.uid = uid
+        self._clearance = Clearance[clearance]
 
     @property
     def clearance(self) -> Clearance:
         return self._clearance
 
-    @property
-    def current_location(self) -> str:
-        return self._location
 
-    @property
-    def current_skills(self) -> Skills:
-        return self._skills
-
-    @property
-    def email_address(self) -> str:
-        return self._email
+class Candidate(BaseClass):
+    def __init__(
+        self,
+        id_string: str,
+        clearance_held: str,
+        year_group: str,
+        prior_departments: str,
+        first_location_preference: str,
+        second_location_preference: str,
+        can_relocate: StrBool,
+        wants_line_management: StrBool,
+        wants_private_office: StrBool,
+        no_defence: StrBool,
+        no_immigration: StrBool,
+        preferred_office_attendance: str,
+        skills_sought: str,
+    ):
+        super().__init__(id_string, clearance_held)
+        self.can_relocate = bool(can_relocate)
+        self.first_preference_location = first_location_preference
+        self.second_preference_location = second_location_preference
+        self.year_group = int(year_group)
+        self.prior_departments = set(prior_departments.split(","))
+        self.wants_line_management = bool(wants_line_management)
+        self.wants_private_office = bool(wants_private_office)
+        self.no_defence = bool(no_defence)
+        self.no_immigration = bool(no_immigration)
+        self.preferred_office_attendance = preferred_office_attendance
+        self.skills_sought: Sequence[str] = skills_sought.split(",")
 
 
 class Role(BaseClass):
-    def __init__(self, name: str, clearance_required: Clearance, location: str, skill_increase: Skills,
-                 owner_email_address: str):
-        super().__init__(name, clearance_required, location, skill_increase, owner_email_address)
+    def __init__(
+        self,
+        name: str,
+        clearance_required: Clearance,
+        location: str,
+        skill_increase: Skills,
+        owner_email_address: str,
+    ):
+        super().__init__(
+            name, clearance_required, location, skill_increase, owner_email_address
+        )
 
     @property
     def location(self) -> str:
