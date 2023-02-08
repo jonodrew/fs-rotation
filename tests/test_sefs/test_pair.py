383e52b6d1c7e4d4c43863722dd00ef86cb070a5
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-02-08 14:11:14 +0000

    Refactor out fixtures

--- tests/test_sefs/test_pair.py
+++ tests/test_sefs/test_pair.py
@@ -12,24 +12,37 @@ def test_when_pair_created_scoring_method_added_to_parent_class():
     assert BasePair._score_location.__name__ in p.scoring_method_names
 
 
+@pytest.fixture
+def sefs_candidate(random_candidate_dict):
+    c = SefsCandidate(not_required="Broad Thinking", **random_candidate_dict())
+    c.primary_skill = "Developing the GSE community"
+    c.secondary_skill = "Building and Applying Knowledge"
+    return c
+
+
+@pytest.fixture
+def sefs_role(random_role_dict):
+    return SefsRole(
+        broad_thinking="A",
+        building_applying="P",
+        communicating="A",
+        oversight="A",
+        developing="P",
+        **random_role_dict()
+    )
+
+
 class TestScoreSkill:
     @pytest.mark.parametrize(["year_group", "score"], [(1, 0), (2, 36)])
     def test_when_year_all_skills_match_scored_is_correct(
-        self, random_candidate_dict, random_role_dict, year_group, score
+        self, sefs_candidate, sefs_role, year_group, score
     ):
-        c = SefsCandidate(not_required="Broad Thinking", **random_candidate_dict())
+        c = sefs_candidate
         c.primary_skill = "Developing the GSE community"
         c.secondary_skill = "Building and Applying Knowledge"
         c.year_group = year_group
 
-        r = SefsRole(
-            broad_thinking="A",
-            building_applying="P",
-            communicating="A",
-            oversight="A",
-            developing="P",
-            **random_role_dict()
-        )
+        r = sefs_role
         p = SefsPair()
         assert p.score == 0
         p._score_skill(c, r)
