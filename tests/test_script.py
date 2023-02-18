6060057c423d183936ad3ea84c0bff86261368bc
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-02-18 15:38:25 +0000

    Patch the _check_score test

--- tests/test_script.py
+++ tests/test_script.py
@@ -1,12 +1,15 @@
+from unittest.mock import patch
+
 from fast_stream_22.scripts.process_pairs import conduct_matching
 
 
 def test_conduct_matching():
-    conduct_matching(
-        "CML/2022-11-21 - CML - BIDS.csv",
-        "CML/2022-11-21 - CML - roles.csv",
-        "CML/2022-11-21 - CML - candidates.csv",
-        senior_first=True,
-        specialism=None,
-    )
-    assert True
+    with patch("fast_stream_22.matching.pair.Pair._check_score"):
+        conduct_matching(
+            "CML/2022-11-21 - CML - BIDS.csv",
+            "CML/2022-11-21 - CML - roles.csv",
+            "CML/2022-11-21 - CML - candidates.csv",
+            senior_first=True,
+            specialism=None,
+        )
+        assert True
