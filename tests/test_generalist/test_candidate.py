3c79d6bb1ffe44928b56dffa98f277a588338f63
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-02-21 15:30:52 +0000

    Create a shiny new fixture

--- tests/test_generalist/test_candidate.py
+++ tests/test_generalist/test_candidate.py
@@ -6,18 +6,23 @@ from fast_stream_22.matching.generalist.models import GeneralistCandidate
 
 
 @pytest.fixture
-def generalist_candidate(random_candidate_dict):
-    return GeneralistCandidate(
-        "Primary Anchor",
-        "Secondary Anchor",
-        "One",
-        "Two",
-        "Three",
-        "Four",
-        "Five",
-        travel_requirements="I can travel nationally",
-        **random_candidate_dict()
-    )
+def generalist_candidate_dict(random_candidate_dict) -> dict:
+    return {
+        **random_candidate_dict(),
+        "primary_anchor_seeking": "Primary Anchor",
+        "secondary_anchor_seeking": "Secondary Anchor",
+        "dept_pref_1": "0",
+        "dept_pref_2": "1",
+        "dept_pref_3": "2",
+        "dept_pref_4": "3",
+        "dept_pref_5": "4",
+        "travel_requirements": "I can travel nationally",
+    }
+
+
+@pytest.fixture
+def generalist_candidate(generalist_candidate_dict):
+    return GeneralistCandidate(**generalist_candidate_dict)
 
 
 def test_year_group_ending_m_sets_secondment_flag(generalist_candidate):
