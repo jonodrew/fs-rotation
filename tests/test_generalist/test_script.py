605eb69fc2bf9b64247c82666d45a326534195c4
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-02-23 09:46:07 +0000

    Run the test in junior-to-senior order
    This is currently the only way that it works!

---
+++ tests/test_generalist/test_script.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch
+
+from fast_stream_22.scripts.process_pairs import conduct_matching
+
+
+def test_can_match_generalists_with_csv_data():
+    with patch("fast_stream_22.matching.generalist.models.GeneralistPair._check_score"):
+        assert conduct_matching(
+            bid_file="tests/test_generalist/test_bids.csv",
+            role_file="tests/test_generalist/test_roles.csv",
+            candidate_file="tests/test_generalist/test_candidates.csv",
+            senior_first=False,
+            specialism="generalist",
+        )
